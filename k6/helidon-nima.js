0647fddee671067e157a6cb59cca1670abedf6a9
shukawam <shukawam@gmail.com>
2023-02-03 18:37:30 +0900

    Added mvnw

--- k6/helidon-nima.js
+++ k6/helidon-nima.js
@@ -2,11 +2,10 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-    vus: 50,
-    duration: '30s'
+    vus: 150,
+    duration: '15s'
 }
 
 export default function () {
-    http.get('http://helidon-nima-client.shukawam.me/client/say');
-    sleep(1)
+    const res = http.get('http://helidon-nima-client.shukawam.me/client/say');
 }
